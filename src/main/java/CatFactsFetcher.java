69cc438de773d9c0fe0fa556497ef0bdb5dd680c
Yunonochkaa <156319429+Yunonochkaa@users.noreply.github.com>
2024-11-10 17:29:14 +0500

    CatFactsFetcher.java

---
+++ src/main/java/CatFactsFetcher.java
@@ -0,0 +1,39 @@
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+
+import org.apache.http.util.EntityUtils;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class CatFactsFetcher {
+
+    private static final String URL = "https://raw.githubusercontent.com/netology-code/jd-homeworks/master/http/task1/cats";
+
+    public static void main(String[] args) {
+        try {
+            List<CatFact> catFacts = fetchCatFacts();
+            List<CatFact> filteredFacts = catFacts.stream()
+                    .filter(fact -> fact.getUpvotes() != null && fact.getUpvotes() > 0)
+                    .collect(Collectors.toList());
+
+            filteredFacts.forEach(System.out::println);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static List<CatFact> fetchCatFacts() throws IOException {
+        try (CloseableHttpClient httpClient = HttpClientBuilder.create().build()) {
+            HttpGet request = new HttpGet(URL);
+            try (CloseableHttpResponse response = httpClient.execute(request)) {
+                String json = EntityUtils.toString(response.getEntity());
+                ObjectMapper objectMapper = new ObjectMapper();
+                return objectMapper.readValue(json, objectMapper.getTypeFactory().constructCollectionType(List.class, CatFact.class));
+            }
+        }
+    }
+}
